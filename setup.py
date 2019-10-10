fbdf3a1cc983d481d581f7fe7f4a4f93bc9237c8
Md Nazrul Islam <connect2nazrul@gmail.com>
2019-10-10 13:28:00 +0600

    Back to development: 0.2.1

--- setup.py
+++ setup.py
@@ -77,6 +77,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/nazrulworld/fhirpath_guillotina",
-    version="0.2.0",
+    version="0.2.1.dev0",
     zip_safe=False,
 )
